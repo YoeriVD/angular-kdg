17295fe494ec3e030ce56d8db3bd640e83caead5
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2014-12-08 20:28:59 +0100

    update azure deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -89,16 +89,16 @@ goto :EOF
 
 echo Handling node.js grunt deployment.
 
-# 1. Select node version
+:: 1. Select node version
 selectNodeVersion
 
-# 2. Install npm packages
+:: 2. Install npm packages
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
   eval $NPM_CMD install
   exitWithMessageOnError "npm failed"
 fi
 
-# 3. Install bower packages
+:: 3. Install bower packages
 if [ -e "$DEPLOYMENT_SOURCE/bower.json" ]; then
   eval $NPM_CMD install bower
   exitWithMessageOnError "installing bower failed"
@@ -106,7 +106,7 @@ if [ -e "$DEPLOYMENT_SOURCE/bower.json" ]; then
   exitWithMessageOnError "bower failed"
 fi
 
-# 4. Run grunt
+:: 4. Run grunt
 if [ -e "$DEPLOYMENT_SOURCE/Gruntfile.js" ]; then
   eval $NPM_CMD install grunt-cli
   exitWithMessageOnError "installing grunt failed"
@@ -114,7 +114,7 @@ if [ -e "$DEPLOYMENT_SOURCE/Gruntfile.js" ]; then
   exitWithMessageOnError "grunt failed"
 fi
 
-# 5. KuduSync to Target
+:: 5. KuduSync to Target
 "$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE/dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
 exitWithMessageOnError "Kudu Sync to Target failed"
 
